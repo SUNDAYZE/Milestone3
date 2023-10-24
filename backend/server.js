8e805a94eb9b9ab90e4b118159a8cd994114c491
Heriberto Delgado Jr <heribertodelgado92@gmail.com>
2023-10-23 19:35:58 -0700

    Connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -17,9 +17,18 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port , process.env.PORT')
-})
+
+
 
